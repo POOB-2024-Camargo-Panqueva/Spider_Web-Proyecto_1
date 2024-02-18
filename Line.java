b717cb87bdfc1263e8e4e4c613bb028fa0dd4dba
t-hash06 <tomasfelipe.p@gmail.com>
2024-02-18 18:08:22 -0500

    [CREATE] Line documentation

--- Line.java
+++ Line.java
@@ -7,23 +7,47 @@ public final class Line {
     private final Point end;
     private String color;
 
+    /**
+     * Constructs a new instance of Line with the specified start and end points.
+     * The default color for the line is gray.
+     *
+     * @param start The starting point of the line.
+     * @param end   The ending point of the line.
+     */
     public Line(Point start, Point end) {
         this.start = start;
         this.end = end;
         this.color = "gray";
     }
 
+    /**
+     * Constructs a new instance of Line with the specified start and end points, and color.
+     *
+     * @param start The starting point of the line.
+     * @param end   The ending point of the line.
+     * @param color The color of the line.
+     */
     public Line(Point start, Point end, String color) {
         this.start = start;
         this.end = end;
         this.color = color;
     }
 
+    /**
+     * Draws the line on the canvas.
+     */
     public void draw() {
         Canvas canvas = Canvas.getCanvas();
         canvas.draw(this, this.color, new Line2D.Double(start.getX(), start.getY(), end.getX(), end.getY()));
     }
 
+    /**
+     * Gets a point on the line at a specified scale between 0 and 1.
+     *
+     * @param scale The scale value between 0 and 1 indicating the position along the line.
+     * @return The point on the line at the specified scale.
+     * @throws IllegalArgumentException if the scale is not between 0 and 1.
+     */
     public Point getScaledPoint(double scale) {
         if (scale < 0 || scale > 1) {
             //TODO: Check if this throws an exception or show a message
