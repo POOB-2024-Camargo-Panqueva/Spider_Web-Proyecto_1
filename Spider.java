0a66f2d80a675089b5f86abcce6888cdca6ab5fe
t-hash06 <tomasfelipe.p@gmail.com>
2024-02-18 18:05:43 -0500

    [CREATE] Spider documentation

--- Spider.java
+++ Spider.java
@@ -14,11 +14,20 @@ public class Spider {
 
     private Point position;
 
+    /**
+     * Constructs a new instance of Spider with the given initial position.
+     *
+     * @param position The initial position of the spider.
+     */
     public Spider(Point position) {
         this.position = position;
         favoriteStrands = new HashMap<>();
     }
 
+    /**
+     * Draws the spider on the canvas.
+     * The spider is represented by a black ellipse centered at its current position.
+     */
     public void draw() {
         Canvas canvas = Canvas.getCanvas();
         double xDraw = position.getX() - (double) WIDTH / 2;
@@ -27,10 +36,24 @@ public class Spider {
         canvas.draw(this, "black", new Ellipse2D.Double(xDraw, yDraw, WIDTH, HEIGHT));
     }
 
+    /**
+     * Removes the favorite strand associated with the specified color from the spider.
+     *
+     * @param color The color of the favorite strand to remove.
+     * @return The value of the removed favorite strand, or null if no favorite strand was associated with the specified color.
+     */
     public Integer removeFavoriteStrand(String color) {
         return favoriteStrands.remove(color);
     }
 
+    /**
+     * Adds a favorite strand to the spider with the specified color and value.
+     * If a favorite strand with the same color already exists, it will be replaced with the new value.
+     *
+     * @param color  The color of the favorite strand to add.
+     * @param strand The value of the favorite strand to add.
+     * @return The previous value associated with the specified color, or null if no favorite strand was associated with the color.
+     */
     public Integer addFavoriteStrand(String color, Integer strand) {
         Integer result = favoriteStrands.remove(color);
         if (result == null) {
@@ -40,6 +63,12 @@ public class Spider {
         return result;
     }
 
+    /**
+     * Moves the spider smoothly to the specified new position.
+     * The movement is performed in a straight line at a constant speed.
+     *
+     * @param newPosition The new position to which the spider will move.
+     */
     public void moveTo(Point newPosition) {
         Point2D.Double director = new Point2D.Double(newPosition.getX() - position.getX(), newPosition.getY() - position.getY());
         Point initialPosition = new Point((int) position.getX(), (int) position.getY());
@@ -65,6 +94,12 @@ public class Spider {
         }
     }
 
+    /**
+     * Moves the spider smoothly through a series of positions.
+     * The spider transitions smoothly from its current position to each of the specified positions in sequence.
+     *
+     * @param positions An array of points representing the positions to which the spider will move.
+     */
     public void moveTo(Point[] positions) {
         for (Point position : positions) {
             this.moveTo(position);
