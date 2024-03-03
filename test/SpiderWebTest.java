b0d19dce336ce800a9345089519a97302471e1e4
t-hash06 <tomasfelipe.p@gmail.com>
2024-03-03 16:35:09 -0500

    [CREATE] Src & test folders, SpiderWeb test prototype

---
+++ test/SpiderWebTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SpiderWebTest {
+
+    @Test
+    void initialTest() {
+        assertTrue(true);
+    }
+}
