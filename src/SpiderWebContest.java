2a90e452de58c94db3fe7794674d90ed31516194
t-hash06 <tomasfelipe.p@gmail.com>
2024-04-04 19:11:41 -0500

    [UPDATE] Refactor SpiderWebContest to more legible variable names

--- src/SpiderWebContest.java
+++ src/SpiderWebContest.java
@@ -2,55 +2,53 @@ import java.util.ArrayList;
 
 public class SpiderWebContest {
 
-    private int nextIndex(int x, int c, int n) {
-        return (x + c) % n;
+    private int nextIndex(int next, int current, int size) {
+        return (next + current) % size;
     }
 
-    private int previousIndex(int x, int c, int n) {
-        return (x - c + n) % n;
+    private int previousIndex(int next, int current, int size) {
+        return (next - current + size) % size;
     }
 
-    private void update(int[] c, int x, int y) {
-        x += 1;
+    private void update(int[] strandCounts, int first, int second) {
+        first += 1;
 
-        while (x < c.length) {
-            c[x] += y;
-            x += x & -x;
+        while (first < strandCounts.length) {
+            strandCounts[first] += second;
+            first += first & -first;
         }
     }
 
-    private int queryCounter(int[] c, int x) {
+    private int queryCounter(int[] strandCounts, int x) {
         int res = 0;
         x += 1;
 
         while (x > 0) {
-            res += c[x];
+            res += strandCounts[x];
             x -= x & -x;
         }
 
         return res;
     }
 
-    private void processUpdates(int[] c, int l, int r, int x) {
-        update(c, l, x);
-        update(c, r + 1, -x);
+    private void processUpdates(int[] strandCounts, int left, int right, int x) {
+        update(strandCounts, left, x);
+        update(strandCounts, right + 1, -x);
     }
 
-    private void processWork(int[] c, int l, int r, int n) {
-        if (l <= r) {
-            processUpdates(c, l, r, -1);
+    private void processWork(int[] strandCounts, int left, int right, int size) {
+        if (left <= right) {
+            processUpdates(strandCounts, left, right, -1);
         } else {
-            processUpdates(c, l, n - 1, -1);
-            processUpdates(c, 0, r, -1);
+            processUpdates(strandCounts, left, size - 1, -1);
+            processUpdates(strandCounts, 0, right, -1);
         }
     }
 
     public ArrayList<Integer> solve(int numOfStrands, int favoriteStrand, int[][] bridges) {
-        int n = numOfStrands;
-        int m = bridges.length;
-        int s = favoriteStrand - 1;
+        int prevFavoriteStrand = favoriteStrand - 1;
 
-        int[] strandCounts = new int[n + 1];
+        int[] strandCounts = new int[numOfStrands + 1];
 
         ArrayList<int[]> lines = new ArrayList<>();
 
@@ -61,16 +59,15 @@ public class SpiderWebContest {
             lines.add(new int[]{d, t});
         }
 
-        for (int i = 0; i < n; i++) {
-            processUpdates(strandCounts, i, i, Math.min(Math.abs(s - i), n - Math.abs(s - i)));
+        for (int i = 0; i < numOfStrands; i++) {
+            processUpdates(strandCounts, i, i, Math.min(Math.abs(prevFavoriteStrand - i), numOfStrands - Math.abs(prevFavoriteStrand - i)));
         }
 
         lines.sort((a, b) -> Integer.compare(b[0], a[0]));
 
         for (int[] line : lines) {
-            int d = line[0];
             int target = line[1];
-            int nx = nextIndex(target, 1, n);
+            int nx = nextIndex(target, 1, numOfStrands);
             int v1 = queryCounter(strandCounts, target);
             int v2 = queryCounter(strandCounts, nx);
 
@@ -82,36 +79,36 @@ public class SpiderWebContest {
 
             if (v1 > v2) {
                 processUpdates(strandCounts, target, target, -1);
-                int cur = 0;
+                int current = 0;
 
                 for (int i = 18; i >= 0; i--) {
-                    if (cur + (1 << i) <= n - 2 && queryCounter(strandCounts, previousIndex(target, cur + (1 << i), n)) == v1 + cur + (1 << i)) {
-                        cur += 1 << i;
+                    if (current + (1 << i) <= numOfStrands - 2 && queryCounter(strandCounts, previousIndex(target, current + (1 << i), numOfStrands)) == v1 + current + (1 << i)) {
+                        current += 1 << i;
                     }
                 }
 
-                if (cur != 0) {
-                    processWork(strandCounts, previousIndex(target, cur, n), previousIndex(target, 1, n), n);
+                if (current != 0) {
+                    processWork(strandCounts, previousIndex(target, current, numOfStrands), previousIndex(target, 1, numOfStrands), numOfStrands);
                 }
 
-                if (queryCounter(strandCounts, nextIndex(nx, 1, n)) != v2 - 1) {
+                if (queryCounter(strandCounts, nextIndex(nx, 1, numOfStrands)) != v2 - 1) {
                     processUpdates(strandCounts, nx, nx, 1);
                 }
             } else {
                 processUpdates(strandCounts, nx, nx, -1);
-                int cur = 0;
+                int current = 0;
 
                 for (int i = 18; i >= 0; i--) {
-                    if (cur + (1 << i) <= n - 2 && queryCounter(strandCounts, nextIndex(nx, cur + (1 << i), n)) == v2 + cur + (1 << i)) {
-                        cur += 1 << i;
+                    if (current + (1 << i) <= numOfStrands - 2 && queryCounter(strandCounts, nextIndex(nx, current + (1 << i), numOfStrands)) == v2 + current + (1 << i)) {
+                        current += 1 << i;
                     }
                 }
 
-                if (cur != 0) {
-                    processWork(strandCounts, nextIndex(nx, 1, n), nextIndex(nx, cur, n), n);
+                if (current != 0) {
+                    processWork(strandCounts, nextIndex(nx, 1, numOfStrands), nextIndex(nx, current, numOfStrands), numOfStrands);
                 }
 
-                if (queryCounter(strandCounts, previousIndex(target, 1, n)) != v1 - 1) {
+                if (queryCounter(strandCounts, previousIndex(target, 1, numOfStrands)) != v1 - 1) {
                     processUpdates(strandCounts, target, target, 1);
                 }
             }
@@ -119,77 +116,10 @@ public class SpiderWebContest {
 
         ArrayList<Integer> result = new ArrayList<>();
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < numOfStrands; i++) {
             result.add(queryCounter(strandCounts, i));
         }
+
         return result;
     }
-
-//    public void simulate(int initialStrand, int finalStrand) {
-//        bridges.sort((Bridge b1, Bridge b2) -> b2.getDistance() - b1.getDistance());
-//        HashMap<Integer, ArrayList<Bridge>> bridgesByStrands = new HashMap<>();
-//
-//        for (int i = 0; i < this.strands; i++) {
-//            bridgesByStrands.put(i, new ArrayList<>());
-//        }
-//
-//        for (Bridge bridge : bridges) {
-//            bridgesByStrands.get(bridge.getInitialStrand() - 1).add(bridge);
-//            bridgesByStrands.get(bridge.getFinalStrand() - 1).add(bridge);
-//        }
-//
-//        int currentStrand = finalStrand;
-//        int clockwiseNeighbor = -1;
-//        int counterclockwiseNeighbor = -1;
-//        int currentRadio = this.radio;
-//        ArrayList<Point> movementPoints = new ArrayList<>();
-//
-//        movementPoints.add(strandLines.get(finalStrand).getEnd());
-//
-//        while(true){
-//            clockwiseNeighbor = currentStrand - 1;
-//            counterclockwiseNeighbor = currentStrand + 1;
-//            if (currentStrand == 1){clockwiseNeighbor = strands;}
-//            else if (currentStrand == strands) {counterclockwiseNeighbor = 1;}
-//
-//            int distanceToNearestBridge = bridgesByStrands.get(currentStrand).get(0).getDistance();
-//
-//            ArrayList<Bridge> possibleClockwiseNeighborsInRange = new ArrayList<Bridge>();
-//            ArrayList<Bridge> possibleCounterclockwiseNeighborsInRange = new ArrayList<Bridge>();
-//
-//            int index = 0;
-//            while (true){
-//                if(bridgesByStrands.get(clockwiseNeighbor).isEmpty()){
-//                    break;
-//                }
-//                Bridge possibleNeighbor = bridgesByStrands.get(clockwiseNeighbor).get(index);
-//
-//                if(possibleNeighbor.getDistance() < distanceToNearestBridge){
-//                    possibleClockwiseNeighborsInRange.add(possibleNeighbor);
-//                } else {
-//                    break;
-//                }
-//                index++;
-//            }
-//
-//            index = 0;
-//            while (true){
-//                if(bridgesByStrands.get(counterclockwiseNeighbor).isEmpty()){
-//                    break;
-//                }
-//                Bridge possibleNeighbor = bridgesByStrands.get(counterclockwiseNeighbor).get(index);
-//
-//                if(possibleNeighbor.getDistance() < distanceToNearestBridge){
-//                    possibleCounterclockwiseNeighborsInRange.add(possibleNeighbor);
-//                } else {
-//                    break;
-//                }
-//                index++;
-//            }
-//
-//
-//        }
-//
-//    };
-
 }
